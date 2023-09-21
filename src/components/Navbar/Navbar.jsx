694938433b8192b6a24ad8fa7dada99abdcfeac8
shivanshsoni1234 <127824432+shivanshsoni1234@users.noreply.github.com>
2023-09-21 11:09:16 +0530

    added logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import Logo from "../../assets/Logo.png";
+import Logo from "../../assets/logo_white_1.png";
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar mb-5  lg:px-20 justify-center">
+      <div className="navbar mb-5  lg:px-20 justify-center items-center">
         <div className="flex-1">
-          <img className="w-44 absolute mt-5" src={Logo} alt="" />
+          <img className="w-40 mt-2 " src={Logo} alt="" />
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-1 text-black">
+          <ul className="menu menu-horizontal px-1 text-white">
             <li>
-              <a href="/#">Login</a>
+              <a href="tap-n.in">About Us</a>
             </li>
 
             <li>
-              <a href="/#">Create Account</a>
+              <a href="tap-n.in/auth/signup">Create Account</a>
             </li>
           </ul>
         </div>
